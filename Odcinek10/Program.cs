389898d7b180095408e8da2f68b79a937dbfae3b
Andrzej Gajewski <gajewskiandrzej@hotmail.com>
2018-10-28 13:12:00 +0100

    klasa a zmienna

---
+++ Odcinek10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odcinek10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentDiary diary = new StudentDiary();
+            diary.AddRating(3);
+            diary.AddRating(5);
+
+            StudentDiary diary2 = diary;
+            diary2.AddRating(4);
+
+            StudentDiary diary3 = diary2;
+            diary3.AddRating(8);
+        }
+    }
+}
